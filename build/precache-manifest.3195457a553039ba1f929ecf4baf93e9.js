44e83917e50f22d0bbcec86ef68fc4ece27b43dd
Krzysztof Król <krzysztof.krol@royalcode.eu>
2020-12-20 13:49:54 +0100

    new domain

---
+++ build/precache-manifest.3195457a553039ba1f929ecf4baf93e9.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "adbb965e1cc05d3d8815446d3775adae",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6ec06ae0ea5c8b4798f6",
+    "url": "/static/css/main.383bc6b3.chunk.css"
+  },
+  {
+    "revision": "2f2fbb6046ecd68db859",
+    "url": "/static/js/2.e5c115fc.chunk.js"
+  },
+  {
+    "revision": "35d21a0a1527db2529a0842f23266b33",
+    "url": "/static/js/2.e5c115fc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6ec06ae0ea5c8b4798f6",
+    "url": "/static/js/main.7eb3eddf.chunk.js"
+  },
+  {
+    "revision": "dc87f59bab162baa05a8",
+    "url": "/static/js/runtime-main.2fa31ae5.js"
+  },
+  {
+    "revision": "df07465d32c150c9a364b4e5d0814456",
+    "url": "/static/media/bg-start.df07465d.jpg"
+  },
+  {
+    "revision": "04c6109b0eb3f754ff08d46bada63077",
+    "url": "/static/media/google.04c6109b.png"
+  }
+]);
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/smart-evolution/precache-manifest.75139a8cdd3bdf8780634977d147e5c8.js"
+  "/precache-manifest.3195457a553039ba1f929ecf4baf93e9.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/smart-evolution/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
